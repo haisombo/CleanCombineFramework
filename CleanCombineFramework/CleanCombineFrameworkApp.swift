36858c7b8fcdb19b1d357537fb122fde0866306c
haisombo <haisombo20@gmail.com>
2024-06-25 10:56:50 +0700

    Initial Commit

---
+++ CleanCombineFramework/CleanCombineFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCombineFrameworkApp.swift
+//  CleanCombineFramework
+//
+//  Created by Hai Sombo on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanCombineFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanCombineFramework/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanCombineFramework
+//
+//  Created by Hai Sombo on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
